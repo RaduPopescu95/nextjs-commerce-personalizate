b06423194e9497f6a758ff263e43b4145d84e095
RaduPopescu95 <73418692+RaduPopescu95@users.noreply.github.com>
2024-11-21 18:35:57 +0000

    modificat culori

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,8 +1,8 @@
-import { getMenu } from 'lib/shopify';
+// import { getMenu } from 'lib/shopify';
 import { NavbarClient } from './NavbarClient';
 
 
 export async function Navbar() {
-  const menu = await getMenu('next-js-frontend-header-menu');
-  return <NavbarClient menu={menu} />;
+  // const menu = await getMenu('next-js-frontend-header-menu');
+  return <NavbarClient />;
 }
